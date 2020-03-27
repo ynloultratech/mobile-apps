c0a3399d77fc13f5e47d30eabc505881da24b6e0
Yonel Ceruto <yonelceruto@gmail.com>
2020-03-27 18:00:34 -0400

    fix assets prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
-// const isProd = (process.env.NODE_ENV || 'production') === 'production';
-// const assetPrefix = isProd ? '/mobile-apps' : '';
 
 module.exports = withImages(
   withCSS({
@@ -10,29 +8,8 @@ module.exports = withImages(
         ? process.env.LOCALE_SUBPATHS
         : 'none',
     },
-    // exportPathMap: () => ({
-    //   '/': { page: '/' },
-    //   '/contact': { page: '/contact' },
-    // }),
-    //assetPrefix,
     webpack: (config, options) => {
       cssModules: true,
-      // config.module.rules.push({
-      //   test: /\.(jpe?g|png|svg|gif|ico|webp)$/,
-      //   exclude: config.exclude,
-      //   use: [
-      //     {
-      //       loader: require.resolve("url-loader"),
-      //       options: {
-      //         limit: config.inlineImageLimit,
-      //         fallback: require.resolve("file-loader"),
-      //         publicPath: `${config.assetPrefix}/_next/static/images/`,
-      //         outputPath: `${isProd ? "/mobile-apps/" : ""}static/images/`,
-      //         name: "[name]-[hash].[ext]"
-      //       }
-      //     }
-      //   ]
-      // });
       config.node = {
         fs: 'empty'
       };
