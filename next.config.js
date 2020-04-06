d1bf6183718cada5f8cfe061592916475f64a74d
Yonel Ceruto <yonelceruto@gmail.com>
2020-04-06 19:09:23 -0400

    install next-offline package

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
+const withOffline = require('next-offline')
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 
-module.exports = withImages(
-  withCSS({
-    publicRuntimeConfig: {
-      localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-        ? process.env.LOCALE_SUBPATHS
-        : 'none',
-    },
-    webpack: (config, options) => {
-      cssModules: true,
-      config.node = {
-        fs: 'empty'
-      };
-      return config;
-    },
-  })
+module.exports = withOffline(
+  withImages(
+    withCSS({
+      publicRuntimeConfig: {
+        localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
+          ? process.env.LOCALE_SUBPATHS
+          : 'none',
+      },
+      webpack: (config, options) => {
+        cssModules: true,
+        config.node = {
+          fs: 'empty'
+        };
+        return config;
+      },
+    })
+  )
 );
