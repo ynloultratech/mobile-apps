416f5bca9afbb6ac6bcbcc84867a06f9cc83fb30
Yonel Ceruto <yonelceruto@gmail.com>
2020-04-07 00:33:24 -0400

    Configure service worker

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const withPWA = require('next-pwa');
-const withOffline = require('next-offline');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 
-module.exports = withPWA(withOffline(withImages(withCSS({
+module.exports = withPWA(withImages(withCSS({
   publicRuntimeConfig: {
     localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
       ? process.env.LOCALE_SUBPATHS
@@ -18,6 +17,30 @@ module.exports = withPWA(withOffline(withImages(withCSS({
   },
   pwa: {
     dest: 'static'
+  },
+  workboxOpts: {
+    swDest: 'static/sw.js',
+    runtimeCaching: [
+      {
+        urlPattern: /.png$/,
+        handler: 'CacheFirst'
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      }
+    ]
   }
 })
-)));
+));
