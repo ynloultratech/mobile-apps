93611332618daa4abf959fccd5ba282431b531ca
Yonel Ceruto <yonelceruto@gmail.com>
2020-04-07 00:03:41 -0400

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
+const withPWA = require('next-pwa');
+const withOffline = require('next-offline');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 
-module.exports = withImages(
-  withCSS({
-    publicRuntimeConfig: {
-      localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-        ? process.env.LOCALE_SUBPATHS
-        : 'none',
-    },
-    webpack: (config, options) => {
-      cssModules: true,
-      config.node = {
-        fs: 'empty'
-      };
-      return config;
-    },
-  })
-);
+module.exports = withPWA(withOffline(withImages(withCSS({
+  publicRuntimeConfig: {
+    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
+      ? process.env.LOCALE_SUBPATHS
+      : 'none',
+  },
+  webpack: (config, options) => {
+    true,
+    config.node = {
+      fs: 'empty'
+    };
+    return config;
+  },
+  pwa: {
+    dest: 'static'
+  }
+})
+)));
