f16c3038e2baeb3540409b526e57650f035f15d1
Yonel Ceruto <yonelceruto@gmail.com>
2020-04-09 18:24:48 -0400

    fix carusel responsive styles

--- components/Testimonials/testi-style.js
+++ components/Testimonials/testi-style.js
@@ -7,7 +7,10 @@ const testiStyles = makeStyles(theme => ({
   root: {
     background: theme.palette.type === 'dark' ? darken(theme.palette.primary.light, 0.6) : lighten(theme.palette.primary.light, 0.8),
     minHeight: 400,
-    paddingTop: theme.spacing(60)
+    paddingTop: theme.spacing(60),
+    [theme.breakpoints.up('xl')]: {
+      paddingTop: theme.spacing(30),
+    }
   },
   carouselWrap: {
     position: 'relative',
