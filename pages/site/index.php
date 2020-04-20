7e88902fe97bd022e5fdfa57b1f9d9f1ecd302ff
Yonel Ceruto <yonelceruto@gmail.com>
2020-04-20 12:07:11 -0400

    fix site redirect script

--- pages/site/index.php
+++ pages/site/index.php
@@ -1,12 +1,8 @@
 <?php
 
-if (0 === strpos($_SERVER['REQUEST_URI'], 'site/')) {
-    $merchantId = substr($_SERVER['REQUEST_URI'], 6);
-
-    if (!$merchantId) {
-        header('Location: https://'.$_SERVER['HTTP_HOST'].'/'.$merchantId);
-        exit();
-    }
+if (0 === strpos($_SERVER['REQUEST_URI'], '/site/') && $merchantId = substr($_SERVER['REQUEST_URI'], 6)) {
+    header('Location: https://'.$_SERVER['HTTP_HOST'].'/'.$merchantId);
+    exit();
 }
 
 header('Location: https://'.$_SERVER['HTTP_HOST']);
