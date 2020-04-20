62f493bc74c17a880895717e848c21955379f5f0
Yonel Ceruto <yonelceruto@gmail.com>
2020-04-20 11:04:58 -0400

    update index.php

--- pages/pos/index.php
+++ pages/pos/index.php
@@ -6,4 +6,34 @@ if (strpos($_SERVER['HTTP_HOST'], 'myugoonline.net') !== false) {
     exit();
 }
 
-header('Location: https://pos.paynup.com');
+$merchantId = substr($_SERVER['REQUEST_URI'], 1);
+
+if (!$merchantId) {
+    header('Location: https://pos.paynup.com');
+    exit();
+}
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.paynup.com',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>"{ \"query\": \"query { merchantInfo(identifier: \\\"$merchantId\\\") { name number ... on DealerMerchantInfo { agent { posUrl }}}}\"\n}",
+  CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+));
+$response = curl_exec($curl);
+curl_close($curl);
+$merchantInfo = json_decode($response, true);
+
+if (!isset($merchantInfo['data']['merchantInfo']['agent']['posUrl'])) {
+    header('Location: https://pos.paynup.com');
+    exit();
+}
+
+header('Location: '.$merchantInfo['data']['merchantInfo']['agent']['posUrl'].'?merchant='.$merchantId);
+
